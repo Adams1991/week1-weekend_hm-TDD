6c4f9ba0cd7ac8612dfcda23b0ea3894e4e0d102
Adams1991 <adams121@hotmail.co.uk>
2018-05-25 14:17:30 +0100

    tests 1-7 passing

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,7 +1,38 @@
-def pet_shop_name(name)
- return @pet_shop[:name]
+def pet_shop_name(pet_shop_details)
+ return pet_shop_details[:name]
 end
 
-def total_cash(sum)
-  return @pet_shop[:admin][:total_cash]
+def total_cash(pet_shop_details)
+  return pet_shop_details[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop_details, cash_change)
+  pet_shop_details[:admin][:total_cash] += cash_change
+end
+
+def pets_sold(pet_shop_details)
+  return pet_shop_details[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop_details, pets_sold)
+  pet_shop_details[:admin][:pets_sold] += pets_sold
+end
+
+def stock_count(pet_shop_details)
+  pet_shop_details[:pets].length
+end
+
+# def pets_by_breed(pet_shop_details, breed_to_check)
+#   breed_array = []
+#   for breed in pet_shop_details
+#   breed_to_check == pet_shop_details
+#   end
+# end
+#
+#
+# def find_pet_by_name(pet_shop_details, pet_name)
+#   if pet_shop_details[:pets][:name] == pet_name
+#     return pet_name
+#   else
+#     return nil
+# end
